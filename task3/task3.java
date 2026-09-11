a15213d18f79db53fe0bced771259fcd1ab49a66
minkuza <beatrichexxx@gmail.com>
2026-09-11 13:48:31 +0400

    3 задание

--- task3/task3.java
+++ task3/task3.java
@@ -1,13 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import com.fasterxml.jackson.databind.node.*;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.File; // подключаем класс для работы с файлами
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class Test{
+    public int id;
+    public String title;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public String value;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public ArrayList<Test> values = new ArrayList<>();
+}
+
+class Tests{
+    public ArrayList<Test> tests = new ArrayList<>();
+    public Tests(ArrayList<Test> tests)
+    {
+        this.tests = tests;
+    }
+    public Tests()
+    {
+    }
+}
+
+class Value{
+    public int id;
+    public String value;
+}
+class Values{
+    public ArrayList<Value> values = new ArrayList<>();
+}
+
+
+public class task3 {
+
+    public static void fillTests(Tests tests, Values values)
+    {
+        for (Test t : tests.tests)
+        {
+            for (Value v : values.values)
+            {
+                if (v.id == t.id) t.value = v.value;
+            };
+            if (t.values != null && t.values.size()>0) //если есть вложенность тестов
+            {
+               // for (Test tt : t.values) fillTests(tt, values);
+                fillTests(new Tests(t.values), values);
+            }
+        }
+    }
+    public static void main(String[] args) {
+      //  String valuesFilePath = args[0]; //путь к файлу values
+       // String testsFilePath = args[1];  //путь к файлу tests
+     //   String reportFilePath = args[2];  //путь к файлу report
+        String valuesFilePath = "task3/values.json"; //путь к файлу values
+        String testsFilePath ="task3/tests.json";  //путь к файлу tests
+        String reportFilePath ="task3/report.json";  //путь к файлу tests
+
+       Tests tests = new Tests();
+       ObjectMapper om = new ObjectMapper();
+        try {
+            tests = om.readValue(new File(testsFilePath), tests.getClass());
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Values values = new Values();
+        try {
+            values = om.readValue(new File(valuesFilePath), values.getClass());
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        fillTests(tests, values);
+
+        try {
+            om.writeValue(new File(reportFilePath), tests);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
     }
 }
