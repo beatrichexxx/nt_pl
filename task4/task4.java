54c990f972cddd63a3e8a2fc9c7b0d184cbfd7d7
minkuza <beatrichexxx@gmail.com>
2026-09-13 20:28:15 +0400

    4 задание

--- task4/task4.java
+++ task4/task4.java
@@ -1,13 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class task4 {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Ошибка: не указан путь к файлу.");
+        }
+        String filename = args[0];
+        //String filename = "task4/nums.txt";
+
+        try {
+
+            Scanner scanner= new Scanner(new File(filename));
+            List<Integer> numbers = new ArrayList<>();
+            while(scanner.hasNextDouble()) {
+                numbers.add(scanner.nextInt());
+            }
+            Collections.sort(numbers);
+            int median = numbers.get(numbers.size() / 2);
+            int totalMoves = 0;
+            for (int num : numbers) {
+                totalMoves += Math.abs(num - median);
+            }
+           if (totalMoves <= 20) {
+                System.out.println(totalMoves);
+            } else {
+                System.out.println("Превышено максимальное количество ходов.");
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
     }
+
+
+
 }
